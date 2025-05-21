ca45fed1ac2957d14d1477d493edd51e236c73e8
bogdanbt <bt.tarasenko@gmail.com>
2025-05-21 13:48:16 +0200

    add cors for all origin

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
 const textRoutes = require("./routes/textRoutes");
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/text", textRoutes);
 
